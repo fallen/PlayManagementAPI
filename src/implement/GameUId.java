e611777a9f5bacf202a8938e96874a3b0413679e
Jérémy Cheynet <jeremy@minet.net>
2011-01-24 17:05:36 +0100

    Rajout de la classe GameUId

---
+++ src/implement/GameUId.java
@@ -0,0 +1,18 @@
+package implement;
+
+public class GameUId {
+	private Integer Uid;
+
+	public Integer getUid() {
+		return Uid;
+	}
+
+	public void setUid(Integer uid) {
+		Uid = uid;
+	}
+
+	public GameUId(Integer uid) {
+		Uid = uid;
+	}
+
+}
